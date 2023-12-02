0412ddbe90f85ba40ca3c539b17bf1fb947f1555
Shamim448 <shamim.448@gmail.com>
2023-12-02 10:39:16 +0600

    Create PaymentDetailes Model with property

---
+++ Payment-Details/PaymentAPI/Models/PaymentDetailes.cs
@@ -0,0 +1,11 @@
+﻿namespace PaymentAPI.Models
+{
+    public class PaymentDetailes
+    {
+        public int Id { get; set; }
+        public string CardHolderName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }       
+    }
+}
