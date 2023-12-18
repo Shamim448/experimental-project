77a017a34a813ecaf2a0206218757358a74ae2c2
shamim.448@gmail.com <F@tem@@251093>
2023-12-18 14:44:39 +0600

    create component

--- angularProject/src/app/app.routes.ts
+++ angularProject/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LiveComponent } from './pages/live/live.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { PointTableComponent } from './pages/point-table/point-table.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home",
+        component: HomeComponent
+    },
+    {
+        path:"live",
+        component: LiveComponent
+    },
+    {
+        path:"history",
+        component: HistoryComponent
+    },
+    {
+        path:"point-table",
+        component: PointTableComponent
+    },
+    {
+        path:"**",
+        component: PageNotFoundComponent
+    },
+];
